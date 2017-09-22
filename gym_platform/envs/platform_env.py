fc2349cd05917ba8c6a812b5e85e82d050d3916a
nosyndicate <nosyndicate@gmail.com>
2017-09-22 00:33:50 -0400

    merge PlatformWorld and PlatformEnv

--- gym_platform/envs/platform_env.py
+++ gym_platform/envs/platform_env.py
@@ -12,6 +12,9 @@ from gym.utils import seeding
 import numpy as np
 
 from objects import (
+    Agent,
+    Player,
+    Platform,
     PlatformWorld,
     MAX_WIDTH,
     MAX_DX,
@@ -20,6 +23,15 @@ from objects import (
     GRAVITY,
     MAX_DDX,
     MAX_DY,
+    PLATFORMS_X,
+    PLATFORMS_Y,
+    PLATFORM_HEIGHT,
+    PLATFORMS_WIDTH,
+    GAPS,
+    MAX_HEIGHT,
+    MAX_PLATWIDTH,
+    MAX_GAP,
+    DT
 )
 
 
@@ -42,11 +54,14 @@ class PlatformEnv(gym.Env):
     }
 
     def __init__(self):
-        ''' The entities are set up and added to a space. '''
+        """
+        Create the world and setup the action and observation spaces
+        """
         self._seed()
         self.viewer = None
 
-        self.world = PlatformWorld()
+        # self.world = PlatformWorld()
+        self._create_world()
         self._reset()
 
         # Determine the upperbound and lowerbound of action space
@@ -62,13 +77,13 @@ class PlatformEnv(gym.Env):
         # time * (-MAX_DDX) < diffx < time * (MAX_DY + MAX_DX - dy0)
 
         # Hop bounds
-        hop_time = 2.0 * self.world.player.hop_dy0 / GRAVITY + 1.0
+        hop_time = 2.0 * self.player.hop_dy0 / GRAVITY + 1.0
         hop_lower = np.array([hop_time * (-MAX_DDX)])
-        hop_upper = np.array([hop_time * (MAX_DY + MAX_DX - self.world.player.hop_dy0)])
+        hop_upper = np.array([hop_time * (MAX_DY + MAX_DX - self.player.hop_dy0)])
         # Leap bounds
-        leap_time = 2.0 * self.world.player.leap_dy0 / GRAVITY + 1.0
+        leap_time = 2.0 * self.player.leap_dy0 / GRAVITY + 1.0
         leap_lower = np.array([leap_time * (-MAX_DDX)])
-        leap_upper = np.array([leap_time * (MAX_DY + MAX_DX - self.world.player.leap_dy0)])
+        leap_upper = np.array([leap_time * (MAX_DY + MAX_DX - self.player.leap_dy0)])
 
         self.action_space = spaces.Tuple((
             spaces.Discrete(3),
@@ -97,9 +112,177 @@ class PlatformEnv(gym.Env):
         ])
 
         low = np.array([
-            -self.world.player.size[0], 0.0, 0.0, -ENEMY_SPEED, 0.0, 0.0, 0.0, 0.0, 0.0
+            -self.player.size[0], 0.0, 0.0, -ENEMY_SPEED, 0.0, 0.0, 0.0, 0.0, 0.0
         ])
         self.observation_space = spaces.Box(low, high)
+
+    def _create_world(self):
+        """
+        The entities are setup and added to a space.
+        """
+        self.x_pos = 0.0
+        self.player = Player()
+
+        # Define platforms, we have three of them
+        self.platforms = []
+        for index in range(3):
+            self.platforms.append(Platform(
+                PLATFORMS_X[index], PLATFORMS_Y[index], PLATFORMS_WIDTH[index]))
+
+        # Create enemies, we have two of them
+        self.enemies = []
+        for index in range(2):
+            self.enemies.append(Agent(self.platforms[index]))
+
+        # Used to record the position of all agents
+        self.states = []
+
+    def platform_features(self, state):
+        """
+        This function returns the set of additional feature for
+        determine policy parameters in original code.
+        """
+        xpos = state[0]
+        if xpos < PLATFORMS_WIDTH[0] + GAPS[0]:
+            pos = 0.0
+            wd1 = PLATFORMS_WIDTH[0]
+            wd2 = PLATFORMS_WIDTH[1]
+            gap = GAPS[0]
+            diff = PLATFORMS_Y[1] - PLATFORMS_Y[0]
+        elif xpos < PLATFORMS_WIDTH[0] + GAPS[0] + PLATFORMS_WIDTH[1] + GAPS[1]:
+            pos = PLATFORMS_WIDTH[0] + GAPS[0]
+            wd1 = PLATFORMS_WIDTH[1]
+            wd2 = PLATFORMS_WIDTH[2]
+            gap = GAPS[1]
+            diff = PLATFORMS_Y[2] - PLATFORMS_Y[1]
+        else:
+            pos = PLATFORMS_WIDTH[0] + GAPS[0] + PLATFORMS_WIDTH[1] + GAPS[1]
+            wd1 = PLATFORMS_WIDTH[2]
+            wd2 = 0.0
+            gap = 0.0
+            diff = 0.0
+        return [wd1 / MAX_PLATWIDTH,
+            wd2 / MAX_PLATWIDTH,
+            gap / MAX_GAP,
+            pos / MAX_WIDTH,
+            diff / MAX_HEIGHT]
+
+    def get_state(self):
+        """
+        Returns the representation of the current state.
+        """
+        if self.player.position[0] > self.platforms[1].position[0]:
+            enemy = self.enemies[1]
+        else:
+            enemy = self.enemies[0]
+
+        state = np.array([
+            self.player.position[0],
+            self.player.velocity[0],
+            enemy.position[0],
+            enemy.dx
+        ])
+
+        # Append the extra features
+        extra_features = self.platform_features(state)
+        state = np.append(state, extra_features)
+        return state
+
+    def on_platforms(self):
+        """
+        Checks if the player is on any of the platforms.
+        """
+        for platform in self.platforms:
+            if self.player.on_platform(platform):
+                return True
+        return False
+
+    def perform_action(self, action, dt=DT):
+        """
+        Applies for selected action for the given agent.
+        """
+        if self.on_platforms():
+            if action:
+                action, parameters = action
+                if action == 'jump':
+                    self.player.jump(parameters)
+                elif action == 'run':
+                    self.player.run(parameters, dt)
+                elif action == 'leap':
+                    self.player.leap_to(parameters)
+                elif action == 'hop':
+                    self.player.hop_to(parameters)
+        else:
+            self.player.fall()
+
+    def lower_bound(self):
+        """
+        Returns the lowest height of the platforms.
+        """
+        length = []
+        for platform in self.platforms:
+            length.append(platform.position[1])
+        lower = min(length)
+        return lower
+
+    def right_bound(self):
+        """
+        Returns the edge of the game.
+        """
+        return MAX_WIDTH
+
+    def terminal_check(self, reward=0.0):
+        """
+        Determines if the episode is ended, and the reward.
+        """
+        # If we fall over the platform, episode over.
+        done = self.player.position[1] < self.lower_bound() + PLATFORM_HEIGHT
+
+        # If collide with enemy, episode over.
+        for entity in self.enemies:
+            if self.player.colliding(entity):
+                done = True
+
+        # If we go over the boundary, game over.
+        right = self.player.position[0] >= self.right_bound()
+        if right:
+            reward = (self.right_bound() - self.x_pos) / self.right_bound()
+            done = True
+        return reward, done
+
+    def update(self, action, dt=DT):
+        """
+        Performs a single transition with the given action,
+        then returns the new state and a reward.
+        """
+        self.states.append([self.player.position.copy(),
+                            self.enemies[0].position.copy(),
+                            self.enemies[1].position.copy()])
+
+        self.perform_action(action, dt)
+
+        # Bound the velocity of player if is at floor
+        if self.on_platforms():
+            self.player.ground_bound()
+
+        # Determine which enemy should move
+        if self.player.position[0] > self.platforms[1].position[0]:
+            enemy = self.enemies[1]
+        else:
+            enemy = self.enemies[0]
+
+        # Update the position of agents
+        for entity in [self.player, enemy]:
+            entity.update(dt)
+
+        # Test if we landed at any platform
+        for platform in self.platforms:
+            if self.player.colliding(platform):
+                self.player.decollide(platform)
+                self.player.velocity[0] = 0.0
+        # The reward for that step is the change in x value at that step
+        reward = (self.player.position[0] - self.x_pos) / self.right_bound()
+        return self.terminal_check(reward)
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -107,6 +290,8 @@ class PlatformEnv(gym.Env):
 
     def _step(self, action):
         """
+        Take a full, stabilised update.
+
         Parameters
         ----------
         action (list): First is the discrete action, the rest is the parameters
@@ -115,11 +300,45 @@ class PlatformEnv(gym.Env):
         # TODO (ewei), action dispatcher seems stupid
         action_str = ACTION_LOOKUP[action[0]]
         action_param = action[action[0] + 1]
-        state, reward, end_episode, step = self.world.step((action_str, action_param), self)
-        return state, reward, end_episode, {step}
+        # state, reward, end_episode, step = self.world.step((action_str, action_param), self)
+        done = False
+        step_unfinished = True
+        action = (action_str, action_param)
+        # Record the position before the step for later reward computation
+        self.x_pos = self.player.position[0]
+        step = 0
+        difft = 1.0
+        # Keep updating until we finish the selected action
+        while step_unfinished:
+            self._render()
+            if action_str == 'run':
+                reward, done = self.update(('run', abs(action_param)), DT)
+                difft -= DT
+                step_unfinished = difft > 0
+            elif action_str in ['jump', 'hop', 'leap']:
+                reward, done = self.update(action)
+                step_unfinished = not self.on_platforms()
+                # jump, hop, and leap only perform once, unlike run
+                action = None
+            if done:
+                step_unfinished = False
+            step += 1
+        state = self.get_state()
+        return state, reward, done, {step}
 
     def _reset(self):
-        return self.world.reset()
+        """
+        Reset the position of enemies and player, start
+        a new episodes.
+        """
+        for enemy in self.enemies:
+            enemy.reset()
+        self.player.reset()
+        self.x_pos = 0.0
+
+        # Clear the buffer
+        self.states = []
+        return self.get_state()
 
     def _vertices(self, position, size):
         left, right, top, bottom = \
@@ -142,7 +361,7 @@ class PlatformEnv(gym.Env):
 
             self.viewer = rendering.Viewer(VIEWER_WIDTH, VIEWER_HEIGHT)
             # Plot platforms
-            for platform in self.world.platforms:
+            for platform in self.platforms:
                 position = platform.position
                 size = platform.size
                 vertices = self._vertices(position, size)
@@ -152,11 +371,11 @@ class PlatformEnv(gym.Env):
 
             # Define enemy movement data structure
             self.enemies_trans = []
-            for enemy in self.world.enemies:
+            for enemy in self.enemies:
                 self.enemies_trans.append(rendering.Transform())
 
             # Plot enemies
-            for enemy, enemy_trans in zip(self.world.enemies, self.enemies_trans):
+            for enemy, enemy_trans in zip(self.enemies, self.enemies_trans):
                 position = enemy.start_position
                 size = enemy.size
                 vertices = self._vertices(position, size)
@@ -168,8 +387,8 @@ class PlatformEnv(gym.Env):
                 self.viewer.add_geom(enemy_polygon)
 
             # Plot player
-            position = self.world.player.start_position
-            size = self.world.player.size
+            position = self.player.start_position
+            size = self.player.size
             vertices = self._vertices(position, size)
             player_polygon = rendering.make_polygon(v=vertices, filled=True)
             self.player_trans = rendering.Transform()
@@ -181,14 +400,14 @@ class PlatformEnv(gym.Env):
 
         # Move the enemies
         for index, tran in enumerate(self.enemies_trans):
-            position = self.world.enemies[index].position
-            start_position = self.world.enemies[index].start_position
+            position = self.enemies[index].position
+            start_position = self.enemies[index].start_position
             tran.set_translation(position[0] - start_position[0],
                 position[1] - start_position[1])
 
         # Move the player
-        position = self.world.player.position
-        start_position = self.world.player.start_position
+        position = self.player.position
+        start_position = self.player.start_position
         self.player_trans.set_translation(position[0] - start_position[0],
             position[1] - start_position[1])
 
