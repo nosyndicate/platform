92766eb52940d98be9e8ed76a9f95157eb5f94f3
nosyndicate <nosyndicate@gmail.com>
2017-09-23 17:56:49 -0400

    update setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_platform',
     version='0.1.0',
-    install_requires=['gym']  # And any other dependencies foo needs
+    install_requires=['gym'],  # And any other dependencies foo needs
+    packages=[package for package in find_packages()
+                if package.startswith('gym')],
     )
